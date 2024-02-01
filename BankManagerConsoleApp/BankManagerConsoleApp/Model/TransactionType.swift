da7b58207af20df977ea1328039e69464f2cc5bc
Andy An <andyanappdev@gmail.com>
2024-02-01 18:43:21 +0900

    feat: Extend TransactionType to define transactionTime method

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/TransactionType.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/TransactionType.swift
@@ -8,3 +8,14 @@
 enum TransactionType {
     case deposit, loan
 }
+
+extension TransactionType {
+    func transactionTime() -> Double {
+        switch self {
+        case TransactionType.deposit:
+            return 0.7
+        case TransactionType.loan:
+            return 1.1
+        }
+    }
+}
