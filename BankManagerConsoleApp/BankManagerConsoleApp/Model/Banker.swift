d154030141d60d830dfc83603771a99e7d87f190
Andy An <andyanappdev@gmail.com>
2024-02-05 00:10:07 +0900

    refactor: Refactor Banker for add processCustomerTransaction method

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
@@ -7,12 +7,33 @@
 
 import Foundation
 
-struct Banker {
+class Banker {
+    
+    //MARK: - Property
     let id: UUID
     let transactionType: TransactionType
+    private(set) var isBusy: Bool
     
     init(transactionType: TransactionType) {
         self.id = UUID()
         self.transactionType = transactionType
+        self.isBusy = false
+    }
+    
+    
+    //MARK: - Method
+    func processCustomerTransaction(customer: Customer, message: ConsoleMessage, completion: @escaping () -> Void) {
+//        let dispatchGroup = DispatchGroup()
+        
+//        dispatchGroup.enter()
+        isBusy.toggle()
+        message.taskStartMessage(customerWatingNumber: customer.watingNumber, transactionType: customer.transaction.type)
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + customer.transaction.duration) {
+            message.taskEndMessage(customerWatingNumber: customer.watingNumber, transactionType: customer.transaction.type)
+            self.isBusy.toggle()
+//            dispatchGroup.leave()
+            completion()
+        }
     }
 }
