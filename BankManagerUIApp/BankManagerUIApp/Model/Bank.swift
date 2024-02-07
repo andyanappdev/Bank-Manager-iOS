4f3bc1e2aab4ed0ec4b29b8f93bbf2b7fe26575e
Andy An <andyanappdev@gmail.com>
2024-02-07 13:51:17 +0900

    refactor: Refactor addNewCustomers method in Bank class

--- BankManagerUIApp/BankManagerUIApp/Model/Bank.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/Bank.swift
@@ -14,40 +14,54 @@ final class Bank<Q: QueueProtocol> where Q.Element == Customer {
     private let bankerManager: BankerManager
     private let processingQueue: DispatchQueue
     private var elapsedTime: Double
+    private var accumulatedCustomers: Int
+    private let numberOfCustomersToAdd: Int
     
     init(waitingCustomers: Q,
          bankerManager: BankerManager,
-         processingQueue: DispatchQueue = DispatchQueue.global()
+         processingQueue: DispatchQueue = DispatchQueue.global(),
+         numberOfCustomersToAdd: Int = 10
     ) {
         self.waitingCustomers = waitingCustomers
         self.bankerManager = bankerManager
         self.processingQueue = processingQueue
         self.elapsedTime = 0
+        self.accumulatedCustomers = 0
+        self.numberOfCustomersToAdd = numberOfCustomersToAdd
     }
     
     
     //MARK: - Method
     func open(completion: @escaping () -> Void) {
-        createCustomers()
+        addNewCustomers()
         
         processCustomerTransaction {
-//            self.closeBank(completion: completion)
+            //            self.closeBank(completion: completion)
             self.bankerManager.resetManager()
         }
     }
     
-    private func createCustomers() {
-        let numberOfCustomers = Int.random(in: 10...30)
-        
-        for number in 1...numberOfCustomers {
-            let transactionType: TransactionType = Bool.random() ? .deposit : .loan
-            let transaction = Transaction(type: transactionType, duration: transactionType.transactionTime())
-            
-            let customer = Customer(watingNumber: number, transaction: transaction)
-            
-            waitingCustomers.enqueue(customer)
+    func addNewCustomers() {
+        for _ in 1...numberOfCustomersToAdd {
+            let customer = createNewCustomer()
+            enqueueCustomer(customer: customer)
         }
     }
+    
+    private func createNewCustomer() -> Customer {
+        let transactionType: TransactionType = Bool.random() ? .deposit : .loan
+        let transaction = Transaction(type: transactionType, duration: transactionType.transactionTime())
+        let customer = Customer(watingNumber: accumulatedCustomers + 1, transaction: transaction)
+        accumulatedCustomers += 1
+        return customer
+    }
+    
+    private func enqueueCustomer(customer: Customer) {
+        waitingCustomers.enqueue(customer)
+    }
+    
+    
+    
     
     private func processCustomerTransaction(completion: @escaping () -> Void) {
         let dispatchGroup = DispatchGroup()
@@ -70,15 +84,15 @@ final class Bank<Q: QueueProtocol> where Q.Element == Customer {
         
         let endTime = DispatchTime.now()
         elapsedTime = Double(endTime.uptimeNanoseconds - startTimes.first!.uptimeNanoseconds) / 1_000_000_000
-
+        
         
         dispatchGroup.notify(queue: processingQueue) {
             completion()
         }
     }
     
-//    private func closeBank(completion: @escaping () -> Void) {
-//        message.bankClosingMessage(totalCustomersProcessed: bankerManager.totalCustomersProcessed, totalProcessingTime: elapsedTime)
-//        completion()
-//    }
+    //    private func closeBank(completion: @escaping () -> Void) {
+    //        message.bankClosingMessage(totalCustomersProcessed: bankerManager.totalCustomersProcessed, totalProcessingTime: elapsedTime)
+    //        completion()
+    //    }
 }
